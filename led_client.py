ac97d7fcaa2f41e95b4a43b689bc3f2576b7a26b
Kevin McAleer <kevinmcaleer@gmail.com>
2022-08-25 16:18:09 +0100

    added json parser

--- led_client.py
+++ led_client.py
@@ -3,18 +3,21 @@
 from easy_comms import Easy_comms
 from time import sleep
 from machine import Pin
+import json
 
 com1 = Easy_comms(0,9600)
 led = Pin("LED", Pin.OUT, value=0)
 
 while True:
     message = com1.read()
-    if message is not None:
-        if message['command'] == 'blink':
-            if message['args'] == 'on':
-                led.on()
-            if message['args'] == 'off':
-                led.off()
-    sleep(0.01)
-        
-
+    try:
+        command = json.loads(message)
+        if command is not None:
+            if command['command'] == 'blink':
+                if command['args'] == 'on':
+                    led.on()
+                if command['args'] == 'off':
+                    led.off()
+        sleep(0.01)
+    except Exception as e:
+        print(f'error: {e}')
