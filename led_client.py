d701071430dcdfdb4c1a4471e7fbec16311396aa
Kevin McAleer <kevinmcaleer@gmail.com>
2022-08-25 16:22:23 +0100

    resequenced logic

--- led_client.py
+++ led_client.py
@@ -10,14 +10,14 @@ led = Pin("LED", Pin.OUT, value=0)
 
 while True:
     message = com1.read()
-    try:
-        command = json.loads(message)
-        if command is not None:
+    if message is not None:
+        try:
+            command = json.loads(message)
             if command['command'] == 'blink':
                 if command['args'] == 'on':
                     led.on()
                 if command['args'] == 'off':
                     led.off()
-        sleep(0.01)
-    except Exception as e:
-        print(f'error: {e}')
+            sleep(0.01)
+        except Exception as e:
+            print(f'error: {e}')
