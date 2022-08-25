32a3f9cf43cee22ce4918b45e24df41c7f5570cb
Kevin McAleer <kevinmcaleer@gmail.com>
2022-08-25 16:15:44 +0100

    undo readline

--- led_client.py
+++ led_client.py
@@ -8,7 +8,7 @@ com1 = Easy_comms(0,9600)
 led = Pin("LED", Pin.OUT, value=0)
 
 while True:
-    message = com1.readline()
+    message = com1.read()
     if message is not None:
         if message['command'] == 'blink':
             if message['args'] == 'on':
