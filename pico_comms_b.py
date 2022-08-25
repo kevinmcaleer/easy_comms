57eb4314e63c54a063601d3495ea3b9384fd61ed
Kevin McAleer <kevinmcaleer@gmail.com>
2022-08-25 11:55:01 +0100

    updated comms b

---
+++ pico_comms_b.py
@@ -0,0 +1,20 @@
+# Pico_comms_b
+# Sends commands and listens to responses from pico_comms_a
+
+from easy_comms import Easy_comms
+from time import sleep
+
+com1 = Easy_comms(uart_id=0, baud_rate=9600)
+com1.start()
+
+count = 0
+while True:
+    # send a message
+    com1.send(f'hello, {count}')
+
+    # check for messages
+    message = com1.read()
+    if message is not None:
+        print(message.decode('utf-8'))
+    sleep(1)
+    count += 1
